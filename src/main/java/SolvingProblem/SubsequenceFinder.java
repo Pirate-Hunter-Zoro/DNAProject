abf5b7b1d4bd1c13ba19279d8e33d9d2e865d4a9
Mikey Ferguson <mikeyferg49@gmail.com>
2022-10-23 10:51:10 -0400

    Started refactoring pair list construction

--- src/main/java/SolvingProblem/SubsequenceFinder.java
+++ src/main/java/SolvingProblem/SubsequenceFinder.java
@@ -23,21 +23,14 @@ public interface SubsequenceFinder {
      * All implementors of this Interphase have access to this method
      * Class method to find the respective indices from each string of the two strings' longest common subsequence
      * @param backTracker {int[][]}
-     * @param s1 {@link String}
-     * @param s2 {@link String}
      * @return {@link ArrayList<Pair>}
      */
-    public static ArrayList<Pair> findSubsequencePositions(int[][] backTracker, String s1, String s2){
+    public static ArrayList<Pair> findSubsequencePositions(int[][] backTracker){
         // traceback the table
-        Stack<Character> subsequence = traceBack(backTracker, s1);
-
-        // unwind the stack to get the longest common subsequence
-        String lCS = "";
-        while (!subsequence.isEmpty())
-            lCS += subsequence.pop();
+        Stack<Pair> subsequence = traceBack(backTracker);
 
         // to be filled up after we have the 2D back-tracking array
-        ArrayList<Pair> pairs = createPairList(lCS, s1 ,s2);
+        ArrayList<Pair> pairs = createPairList(subsequence);
 
         // the ArrayList of Pairs
         return pairs;
@@ -47,12 +40,11 @@ public interface SubsequenceFinder {
      * No need for anything besides SubsequenceFinder to use this method
      * Given a String for reference, backtracks along a table to construct the longest common subsequence between the given String and whatever other String produced the table
      * @param backTracker {@link int[][]}
-     * @param s {@link String}
-     * @return {@link Stack<Character>}
+     * @return {@link Stack<Pair>}
      */
-    private static Stack<Character> traceBack(int[][] backTracker, String s){
+    private static Stack<Pair> traceBack(int[][] backTracker){
         // create the stack
-        Stack<Character> subsequence = new Stack<>();
+        Stack<Pair> subsequence = new Stack<>();
 
         // to help loop effectively
         int j=backTracker.length-1;
@@ -63,7 +55,7 @@ public interface SubsequenceFinder {
             if (backTracker[j][i] == UP_LEFT){
                 j--;
                 i--;
-                subsequence.push(s.charAt(j));
+                subsequence.push(new Pair(j, i));
             } else if (backTracker[j][i] == LEFT)
                 i--;
             else
@@ -84,32 +76,20 @@ public interface SubsequenceFinder {
 
     /**
      * Given the common String between two strings, return the list of pairs of indices that match accordingly
-     * @param common {@link String}
-     * @param s1 {@link String}
-     * @param s2 {@link String}
-     * @return pairs {@link ArrayList<Pair>}
+     * @param pairs {@link Stack<Pair>}
+     * @return pairList {@link ArrayList<Pair>}
      * All implementers of this interphase receive this method
      */
-    public static ArrayList<Pair> createPairList(String common, String s1, String s2){
+    public static ArrayList<Pair> createPairList(Stack<Pair> pairs){
         // reconstruct the respective indices within s1 and s2 that create the common String
-        ArrayList<Pair> pairs = new ArrayList<>();
+        ArrayList<Pair> pairList = new ArrayList<>();
 
-        // starting positions for searching - store as mutable object so that they can be modified
-        int[] lastJ = {-1};
-        int[] lastI = {-1};
-        for (int k=0; k<common.length(); k++){
-            // find the next occurrence of the character in s1 starting at index lastJ (and update lastJ)
-            int s1Idx = findIndexAt(s1, common.charAt(k), lastJ);
+        // unwind the stack into a list of Pairs
+        while (!pairs.isEmpty())
+            pairList.add(pairs.pop());
 
-            // same for s2 and lastI
-            int s2Idx = findIndexAt(s2, common.charAt(k), lastI);
-
-            // insert the index pair
-            pairs.add(new Pair(s1Idx, s2Idx));
-        }
-
-        // return the list of pairs
-        return pairs;
+        // return the list of Pairs
+        return pairList;
     }
 
     /**
--- src/test/java/SolverNeedlemanWunschTest.java
+++ src/test/java/SolverNeedlemanWunschTest.java
@@ -13,23 +13,34 @@ class SolverNeedlemanWunschTest {
     static String s1 = "CACATA";
     static String s2 = "CAGCTA";
 
+    static String s3 = "SIMILARITY";
+    static String s4 = "PILLAR";
+
     /** Our actual solver */
     SolverNeedlemanWunsch solver;
 
     /** To enable easy emplacement into the expected list of Pairs */
     static int[] s1Indices = {0,1,2,4,5};
     static int[] s2Indices = {0,1,3,4,5};
+    static int[] s3Indices = {0,1,3,4,5,6};
+    static int[] s4Indices = {0,1,2,3,4,5};
 
     /** Lists of Pairs containing the indices to use for each String to create the longest common subsequence */
-    static ArrayList<Pair> expected = new ArrayList<>();
-    static ArrayList<Pair> expectedReverse = new ArrayList<>();
+    static ArrayList<Pair> expected1 = new ArrayList<>();
+    static ArrayList<Pair> expectedReverse1 = new ArrayList<>();
+    static ArrayList<Pair> expected2 = new ArrayList<>();
+    static ArrayList<Pair> expectedReverse2 = new ArrayList<>();
 
     /** Pre-test String setup */
     @BeforeEach
     void setUp() {
         for (int k=0; k< s1Indices.length; k++){
-            expected.add(new Pair(s1Indices[k], s2Indices[k]));
-            expectedReverse.add(new Pair(s2Indices[k], s1Indices[k]));
+            expected1.add(new Pair(s1Indices[k], s2Indices[k]));
+            expectedReverse1.add(new Pair(s2Indices[k], s1Indices[k]));
+        }
+        for (int k=0; k< s3Indices.length; k++){
+            expected2.add(new Pair(s3Indices[k], s4Indices[k]));
+            expectedReverse2.add(new Pair(s4Indices[k], s4Indices[k]));
         }
         solver = new SolverNeedlemanWunsch();
     }
@@ -37,11 +48,16 @@ class SolverNeedlemanWunschTest {
     /** A test to ensure that Solver's getLineUp method returns the expected list of Pairs */
     @Test
     void findSubsequencePositions() {
-        ArrayList<Pair> actual = solver.getLineUp(s1, s2);
-        assertEquals(expected, actual);
+        ArrayList<Pair> actual1 = solver.getLineUp(s1, s2);
+        assertEquals(expected1, actual1);
 
-        ArrayList<Pair> actualReverse = solver.getLineUp(s2, s1);
-        assertEquals(expectedReverse, actualReverse);
+        ArrayList<Pair> actualReverse1 = solver.getLineUp(s2, s1);
+        assertEquals(expectedReverse1, actualReverse1);
+
+        ArrayList<Pair> actual2 = solver.getLineUp(s3,s4);
+        ArrayList<Pair> actualReverse2 = solver.getLineUp(s4,s3);
+        assertEquals(expected2, actual2);
+        assertEquals(expectedReverse2, actualReverse2);
     }
 
 }
