15507907f8bde6dbe8a85a96eb843bc7bd55c15a
ael023 <ael023@bucknell.edu>
2022-10-26 23:02:39 -0400

    UI, mostly working

--- src/main/java/UserInterface/UImain.java
+++ src/main/java/UserInterface/UImain.java
@@ -27,22 +27,27 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.stage.FileChooser.ExtensionFilter;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.io.File;
 
 //imports to analyze sequence
-import FileScanner.Reader;
-import SolvingProblem.SolverLCS;
-import SolvingProblem.SolverNeedlemanWunsch;
-import SolvingProblem.SolverSubstring;
+
 
 public class UImain extends Application{
-    private Button btn;
-    private Label lblTime;
-    private Label lblAnalyzeChoice;
+    private Button btnAnalyze;
+    private Button btnQuery;
+    private Button btnDatabase;
 
+    private Label lblAnalyzeChoice;
+    private Label lblQuery;
+    private Label lblDatabase;
+    private Label lblBottom;
+
+    File queryFile = null;
+    File databaseFile = null;
     private ChoiceBox<String> AnalyzeMethodBox;
 
     public static void main(String[] args) {
@@ -78,40 +83,79 @@ public class UImain extends Application{
      * @param root root for our scene
      */
     private void initSceneGraph(VBox root) {
-        //create vbox for
+        //create hboxs for side by side elements
         HBox DropDownBoxNode = new HBox(10);
         DropDownBoxNode.setAlignment(Pos.CENTER);
 
+        HBox QuerySelectNode = new HBox(10);
+        QuerySelectNode.setAlignment(Pos.CENTER);
+
+        HBox DatabaseSelectNode = new HBox(10);
+        DatabaseSelectNode.setAlignment(Pos.CENTER);
+
+        //Create our labels
+        lblQuery = new Label("!NO SOURCE SELECTED!");
+        lblDatabase = new Label("!NO DATABASE SELECTED!");
         lblAnalyzeChoice = new Label();
         lblAnalyzeChoice.setText("Select an Analysis Method:");
 
-        lblTime = new Label();
 
-        //create date/time button
-        btn = new Button();
-        btn.setText("Analyze!");
+        lblBottom = new Label();
+
+        //create our buttons
+        btnQuery = new Button();
+        btnQuery.setText("Select Query File");
+        btnDatabase = new Button();
+        btnDatabase.setText("Select Database File");
+        btnAnalyze = new Button();
+        btnAnalyze.setText("Analyze!");
 
 
         //Creating our dropdown List
         AnalyzeMethodBox = new ChoiceBox<String>();
-        AnalyzeMethodBox.getItems().addAll("ALL METHODS",
-                "Longest Common Substring",
+        AnalyzeMethodBox.getItems().addAll("Longest Common Substring",
                 "Longest Common Subsequence",
-                "Needle-Wunsch Algorithm"
+                "Needle-Wunsch Algorithm",
+                "ALL METHODS"
         );
-        AnalyzeMethodBox.setValue("ALL METHODS"); //setting default value
+        AnalyzeMethodBox.setValue("Longest Common Substring"); //setting default value
 
         //add ChoiceBox + Label to DropDownBoxNode
-        DropDownBoxNode.getChildren().add(lblAnalyzeChoice);
-        DropDownBoxNode.getChildren().add(AnalyzeMethodBox);
+        DropDownBoxNode.getChildren().addAll(lblAnalyzeChoice, AnalyzeMethodBox);
+
+        FileChooser fileChooser = new FileChooser();
+        ExtensionFilter txtExtFilter = new ExtensionFilter("Text Files", "*.txt");
+        ExtensionFilter allExtFilter = new ExtensionFilter("All Files", "*.txt");
+        fileChooser.getExtensionFilters().addAll(txtExtFilter,allExtFilter);
+        //Source Select Node
+        QuerySelectNode.getChildren().addAll(btnQuery, lblQuery);
+        btnQuery.setOnAction(event -> {
+            fileChooser.setTitle("Choose a valid Query File");
+            queryFile = fileChooser.showOpenDialog(null);
+            if (queryFile != null){
+                lblQuery.setText(String.valueOf(queryFile));
+            }
+        });
+        //Database Select Node
+        DatabaseSelectNode.getChildren().addAll(btnDatabase,lblDatabase);
+        btnDatabase.setOnAction(event -> {
+            fileChooser.setTitle("Choose a valid Database File");
+            databaseFile = fileChooser.showOpenDialog(null);
+            if (databaseFile != null){
+                lblDatabase.setText(String.valueOf(databaseFile));
+            }
+        });
 
         //add elements to root
         root.getChildren().add(DropDownBoxNode);
-        root.getChildren().add(btn);
-        root.getChildren().add(lblTime);
+        root.getChildren().add(QuerySelectNode);
+        root.getChildren().add(DatabaseSelectNode);
+        root.getChildren().add(btnAnalyze);
 
-        btn.setOnAction(event -> {
-            lblTime.setText(AnalyzeMethodBox.getValue());
+        root.getChildren().add(lblBottom);
+
+        btnAnalyze.setOnAction(event -> {
+            lblBottom.setText(AnalyzeMethodBox.getValue());   //THIS WILL RUN OUR ANALYZER, with specified value
         });
 
     }
